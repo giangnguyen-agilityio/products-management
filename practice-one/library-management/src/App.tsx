b710805aeb46306955637e38eca917b6b68e1227
Giang Nguyen <giang.nguyen@asnet.com.vn>
2023-05-09 15:14:25 +0700

    Implement the typography component

--- practice-one/library-management/src/App.tsx
+++ practice-one/library-management/src/App.tsx
@@ -1,4 +1,3 @@
-
 function App() {
   return (<>
     ReactJS Practice 1
---
+++ practice-one/library-management/src/components/Typography/Typography.stories.tsx
@@ -0,0 +1,38 @@
+import { StoryFn, Meta } from '@storybook/react';
+
+import Typography from './index';
+
+export default {
+    title: 'Example/Typography',
+    component: Typography,
+    argTypes: {
+        variant: {
+            control: {
+                type: 'select',
+                options: [
+                    'h1',
+                    'h2',
+                    'h3',
+                    'h4',
+                    'h5',
+                    'h6',
+                    'p'
+                ],
+            },
+        },
+        color: {
+            control: {
+                type: 'select',
+                options: ['primary', 'secondary'],
+            },
+        },
+    },
+} as Meta<typeof Typography>;
+
+const Template: StoryFn<typeof Typography> = (args) => <Typography {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+    children: 'This is a paragraph',
+};
+
