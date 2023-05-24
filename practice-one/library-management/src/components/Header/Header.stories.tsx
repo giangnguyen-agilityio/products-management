56d45116decf4eb8d82180c47f9ce045faa04086
Giang Nguyen <giang.nguyen@asnet.com.vn>
2023-05-24 09:04:57 +0700

    Update the format for all the Storybook files

--- practice-one/library-management/src/components/Header/Header.stories.tsx
+++ practice-one/library-management/src/components/Header/Header.stories.tsx
@@ -1,13 +1,13 @@
-import { type StoryFn, type Meta } from '@storybook/react'
-import Header from './index'
+import { type StoryFn, type Meta } from "@storybook/react";
+import Header from "./index";
 
 export default {
-  title: 'Example/Header',
-  component: Header
-} as Meta<typeof Header>
+  title: "Example/Header",
+  component: Header,
+} as Meta<typeof Header>;
 
-const Template: StoryFn<typeof Header> = (args) => <Header {...args} />
-export const Default = Template.bind({})
+const Template: StoryFn<typeof Header> = (args) => <Header {...args} />;
+export const Default = Template.bind({});
 Default.args = {
-  isLogin: false
-}
+  isLogin: false,
+};
