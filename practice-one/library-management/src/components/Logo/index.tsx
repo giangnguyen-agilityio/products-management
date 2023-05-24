22976a992a7835754ff833aad067d1a4a41107c6
Giang Nguyen <giang.nguyen@asnet.com.vn>
2023-05-24 09:12:29 +0700

    Update the Logo component

--- practice-one/library-management/src/components/Logo/index.tsx
+++ practice-one/library-management/src/components/Logo/index.tsx
@@ -11,12 +11,12 @@ const Logo = (props: LogoProps) => {
   const { text, imageSrc, altText, size } = props
   return (
     <div className="logo">
-      {text && <h1>{text}</h1>}
-      {imageSrc && (
+      {(text != null) && <h1>{text}</h1>}
+      {(imageSrc != null) && (
         <img
           src={imageSrc}
           alt={altText}
-          style={{ width: `${size}px`, height: `${size}px` }}
+          style={{ width: `${size ?? 50}px`, height: `${size ?? 50}px` }}
         />
       )}
     </div>
