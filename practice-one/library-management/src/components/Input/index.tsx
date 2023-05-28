22872977594b079a519b457e54f2c3d80f38e1f4
Nguyễn Trường Giang <nguyentruonggiang17t4@gmail.com>
2023-05-29 01:51:43 +0700

    Update the Input component

--- practice-one/library-management/src/components/Input/index.tsx
+++ practice-one/library-management/src/components/Input/index.tsx
@@ -1,48 +1,23 @@
-import React from 'react'
+import React, { type InputHTMLAttributes } from 'react'
 
 // Importing the CSS file for styling
 import './input.css'
 
 // Define the props for the Input component
-interface InputProps {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string
   classNameLabel?: string
-  type: string
-  name: string
-  placeholder?: string
-  className?: string
-  value: string
-  id: string
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
   errorMessage?: string
 }
 
 const Input: React.FC<InputProps> = (props): JSX.Element => {
-  const {
-    classNameLabel,
-    label,
-    type,
-    name,
-    placeholder,
-    className,
-    value,
-    id,
-    onChange,
-    errorMessage
-  } = props
+  const { classNameLabel, label, errorMessage, ...inputProps } = props
   return (
     <div className="input-wrapper">
-      <label htmlFor={id} className={`label ${classNameLabel ?? ''}`}>
-        {label}
-      </label>
+      <label className={`label ${classNameLabel ?? ''}`}>{label}</label>
       <input
-        type={type}
-        name={name}
-        placeholder={placeholder}
-        value={value}
-        className={`input ${className ?? ''}`}
-        id={id}
-        onChange={onChange}
+        className={`input ${inputProps.className ?? ''}`}
+        {...inputProps}
       />
       {errorMessage != null && <p className="error-message">{errorMessage}</p>}
     </div>
