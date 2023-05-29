ba6ce2c92fbdcfcc6775f70618630b8b0b9ee8fb
Giang Nguyen <giang.nguyen@asnet.com.vn>
2023-05-29 17:52:57 +0700

    Create the constants

---
+++ practice-one/library-management/src/constants/action.ts
@@ -0,0 +1,8 @@
+export const ACTION = {
+  // Book
+  SET_BOOK: 'set_book',
+  ADD_NEW_BOOK: 'add_new_book',
+  EDIT_BOOK: 'edit_book',
+  DELETE_BOOK: 'delete_book',
+  RENT_BOOK: 'rent_book'
+}
---
+++ practice-one/library-management/src/constants/api.ts
@@ -0,0 +1,6 @@
+// export const API_URL = process.env.VITE_BASE_URL
+export const API_URL = 'http://localhost:3000'
+
+export enum APIType {
+  BOOKS = 'books',
+}
