ccc1d195d22e4f266fdae19da42be1a70a4ec0ab
Giang Nguyen Truong <giang.nguyen@asnet.com.vn>
2023-06-02 22:34:32 +0700

    Update the Banner component

--- practice-one/library-management/src/components/Banner/index.tsx
+++ practice-one/library-management/src/components/Banner/index.tsx
@@ -1,4 +1,7 @@
-import React, { useState, useCallback, useMemo } from 'react'
+import React, { useState, useCallback, useMemo, useContext } from 'react'
+
+// Importing the Book Context
+import BookContext from '../../store/BookContext'
 
 // Importing the Icon from the React-icons library
 import { FaArrowRight, FaArrowLeft, FaRegHandPointRight } from 'react-icons/fa'
@@ -17,41 +20,57 @@ import { type Book } from '../../types/book'
 // Importing the CSS file for styling
 import './banner.css'
 
-// Define the props for the Banner component
-interface BannerProps {
-  bookList: Book[]
-}
-
-const Banner: React.FC<BannerProps> = (props) => {
-  const { bookList } = props
+const Banner: React.FC = () => {
+  const { state } = useContext(BookContext)
+  const bookList: Book[] = state.books
 
   // State to keep track of the active book index
   const [activeIndex, setActiveIndex] = useState(0)
 
   // Callback function to handle the previous button click
   const handlePreviousBtnClick = useCallback(() => {
-    setActiveIndex((prevIndex) =>
-      prevIndex === 0 ? bookList.length - 1 : prevIndex - 1
-    )
+    if (bookList != null) {
+      setActiveIndex((prevIndex) =>
+        prevIndex === 0 ? bookList.length - 1 : prevIndex - 1
+      )
+    }
   }, [bookList])
 
   // Callback function to handle the next button click
   const handleNextBtnClick = useCallback(() => {
-    setActiveIndex((prevIndex) =>
-      prevIndex === bookList.length - 1 ? 0 : prevIndex + 1
-    )
+    if (bookList != null) {
+      setActiveIndex((prevIndex) =>
+        prevIndex === bookList.length - 1 ? 0 : prevIndex + 1
+      )
+    }
   }, [bookList])
 
   // Callback function to handle the rent button click
-  const handleRentBtnClick = useCallback((id: string) => {
-    window.alert(`${id} was rented`)
+  const handleRentBtnClick = useCallback((id: string | undefined) => {
+    if (id != null) {
+      window.alert(`${id} was rented`)
+    }
   }, [])
 
   // Get the current book based on the active index
-  const currentBook = useMemo(
-    () => bookList[activeIndex],
-    [bookList, activeIndex]
-  )
+  const currentBook = useMemo(() => {
+    if (bookList !== null) {
+      return bookList[activeIndex]
+    }
+    return null
+  }, [bookList, activeIndex])
+
+  // Render the data of Book
+  const renderBookTitle =
+    currentBook?.title !== '' ? currentBook?.title : 'This is the book title'
+  const renderBookAuthor =
+    currentBook?.author !== '' ? currentBook?.author : 'The author of the book'
+  const renderBookPrice =
+    currentBook?.price !== undefined ? currentBook?.price.toFixed(2) : 0
+  const renderBookDescription =
+    currentBook?.description !== ''
+      ? currentBook?.description
+      : 'This is the description'
 
   return (
     <section className="banner-wrapper">
@@ -60,19 +79,19 @@ const Banner: React.FC<BannerProps> = (props) => {
           <div className="banner-info">
             {/* Display the book title */}
             <Typography variant="h2" className="book-title">
-              {currentBook.title}
+              {renderBookTitle}
             </Typography>
             {/* Display the book author */}
             <Typography variant="h3" className="book-author">
-              {currentBook.author}
+              {renderBookAuthor}
             </Typography>
             {/* Display the book price */}
             <Typography variant="h3" className="book-price">
-              {`${currentBook.price.toFixed(2)} $`}
+              {renderBookPrice} $
             </Typography>
             {/* Display the book description */}
             <Typography variant="p" className="book-description">
-              {currentBook.description}
+              {renderBookDescription}
             </Typography>
             {/* Rent button */}
             <Button
@@ -81,21 +100,21 @@ const Banner: React.FC<BannerProps> = (props) => {
               className="rent-btn"
               ariaLabel="Rent button"
               onClick={() => {
-                handleRentBtnClick(currentBook.bookId)
+                handleRentBtnClick(currentBook?.id)
               }}
             >
               Rent this book
               <FaRegHandPointRight />
             </Button>
             {/* Pagination */}
-            <Pagination list={bookList} activeIndex={activeIndex} />
+            <Pagination length={bookList.length} activeIndex={activeIndex} />
           </div>
           <div className="banner-image-wrapper">
             {/* Display the book image */}
             <img
               className="banner-image"
-              src={currentBook.image}
-              alt={currentBook.alt}
+              src={currentBook?.image ?? undefined}
+              alt={currentBook?.alt}
             />
           </div>
           <div className="banner-controls">
