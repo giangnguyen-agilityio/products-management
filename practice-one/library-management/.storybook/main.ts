50d032953b6fa5b5663bdd425474281edd800ab2
Giang Nguyen Truong <giang.nguyen@asnet.com.vn>
2023-06-08 23:24:06 +0700

    Edit the alias path and update the Editorconfig, eslint and prettier file

--- practice-one/library-management/.storybook/main.ts
+++ practice-one/library-management/.storybook/main.ts
@@ -1,17 +1,31 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type {StorybookConfig} from '@storybook/react-vite'
+const tsconfigPaths = require('vite-tsconfig-paths')
+
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
+  core: {
+    builder: "@storybook/builder-vite"
+  },
+  features: {
+    storyStoreV7: true,
+  },
   framework: {
-    name: "@storybook/react-vite",
+    name: '@storybook/react-vite',
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: 'tag',
   },
-};
-export default config;
+  async viteFinal(config) {
+    return {
+      ...config,
+      plugins: [...(config.plugins || []), tsconfigPaths.default()],
+    };
+  },
+}
+export default config
