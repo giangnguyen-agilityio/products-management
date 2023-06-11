6c1bb7a2af938f6ee0ce6bdfadabcbab27708f66
Giang Nguyen Truong <giang.nguyen@asnet.com.vn>
2023-06-11 21:58:48 +0700

    Refactor the Button component

--- practice-one/library-management/src/components/Button/index.tsx
+++ practice-one/library-management/src/components/Button/index.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react'
+import React, {memo} from 'react'
 
 // Importing the CSS file for styling
 import './button.css'
@@ -7,20 +7,20 @@ import './button.css'
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   size?: 'small' | 'medium' | 'large'
   variant?:
-  | 'primary'
-  | 'secondary'
-  | 'default'
-  | 'success'
-  | 'warning'
-  | 'danger'
+    | 'primary'
+    | 'secondary'
+    | 'default'
+    | 'success'
+    | 'warning'
+    | 'danger'
   className?: string
   ariaLabel?: string
   onClick?: () => void
 }
 
-const Button: React.FC<ButtonProps> = (props) => {
+const Button: React.FC<ButtonProps> = props => {
   // Destructure the props
-  const { children, size, variant, className, ariaLabel, onClick, ...rest } =
+  const {children, size, variant, className, ariaLabel, onClick, ...rest} =
     props
 
   return (
