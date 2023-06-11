dc16208cb00a9d8e7f2a37d6c0e8b1ca3704c5e7
Giang Nguyen Truong <giang.nguyen@asnet.com.vn>
2023-06-11 22:05:04 +0700

    Refactor and responsive for the TableHead component

--- practice-one/library-management/src/components/TableHead/index.tsx
+++ practice-one/library-management/src/components/TableHead/index.tsx
@@ -1,30 +1,34 @@
 import React from 'react'
 
 // Importing the Table column type
-import { type TableColumn } from '../../types/hireRequest'
+import {TableColumn} from '@types'
 
 // Importing the CSS file for styling
 import './table-head.css'
+import {getItemInLocalStorage} from '@helpers'
 
 // Define the props for the Table Head component
 interface TableHeadProps {
   columns: TableColumn[]
 }
 
-const TableHead: React.FC<TableHeadProps> = (props) => {
-  const { columns } = props
+const TableHead: React.FC<TableHeadProps> = props => {
+  const {columns} = props
+
+  const isAdmin = getItemInLocalStorage('memberRole') === 'admin'
+
   return (
     <thead className="table-header">
       <tr className="table-header-row">
         {/* Render table header cells for each column */}
-        {columns.map((column) => (
+        {columns.map(column => (
           <th key={column.field} className="table-header-cell">
             {/* Display the header name of the column */}
             {column.headerName}
           </th>
         ))}
         {/* Render the "Action" header cell */}
-        <th className="table-header-cell">Action</th>
+        {isAdmin ? <th className="table-header-cell">Action</th> : null}
       </tr>
     </thead>
   )
