a75d990fc48f057ae9b1187aa48b2a60efe29f0b
Giang Nguyen <giang.nguyen@asnet.com.vn>
2023-06-14 11:34:10 +0700

    Update the constants

---
+++ practice-one/library-management/src/constants/list.ts
@@ -0,0 +1,5 @@
+export const LIST = {
+  ITEMS_PER_PAGE: 4,
+}
+export const HIRED_BOOK_LIMIT = 5
+export const BOOK_HIRE_REQUESTS_LIMIT = 5
--- practice-one/library-management/src/constants/modal.ts
+++ practice-one/library-management/src/constants/modal.ts
@@ -7,6 +7,5 @@ export enum MODAL {
 export const MODAL_TITLE = {
   MODAl_ADD_BOOK_TITLE: 'Add a new book',
   MODAL_EDIT_BOOK_TITLE: 'Edit the book',
-
   MODAL_EDIT_HIRE_REQUEST: 'Edit the hire request',
 }
