8cda13d2c06d820251a6776d2b1656d187e859ab
Giang Nguyen <giang.nguyen@asnet.com.vn>
2023-07-28 10:03:25 +0700

    Update the Unit test for the Typography component

--- practice-one/library-management/src/components/commons/Typography/Typography.test.tsx
+++ practice-one/library-management/src/components/commons/Typography/Typography.test.tsx
@@ -1,18 +1,71 @@
+// Typography.test.tsx
 import React from 'react'
-import {render, screen} from '@testing-library/react'
-import Typography from '.'
+import {render} from '@testing-library/react'
+import Typography, {TypographyProps} from '.'
 
 describe('Typography component', () => {
-  it('renders correctly with the provided props', () => {
-    const {getByText} = render(
-      <Typography variant="h1" color="primary">
-        This is text!
-      </Typography>
-    )
+  const mockTypographyProps: TypographyProps = {
+    variant: 'p',
+    children: 'Test Typography',
+  }
 
-    expect(getByText('This is text!')).toBeInTheDocument()
-    expect(screen.getByRole('heading', {level: 1})).toBeInTheDocument()
-    expect(getByText('This is text!')).toHaveClass('text-h1 text-primary')
-    expect(getByText('This is text!')).toMatchSnapshot()
+  it('should renders a paragraph with default styles when variant is not specified', () => {
+    const {container, getByText} = render(
+      <Typography {...mockTypographyProps} />
+    )
+    const textContent = getByText('Test Typography')
+    expect(textContent.tagName.toLowerCase()).toBe('p')
+    expect(textContent).toHaveClass('text-p')
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should renders different heading elements when variant is specified', () => {
+    const variants: TypographyProps['variant'][] = [
+      'h1',
+      'h2',
+      'h3',
+      'h4',
+      'h5',
+      'h6',
+    ]
+
+    variants.forEach(variant => {
+      const {container} = render(
+        <Typography {...mockTypographyProps} variant={variant} />
+      )
+      const headingElement = container.querySelector(variant)
+
+      expect(headingElement?.tagName).toBe(variant.toUpperCase())
+      expect(container).toMatchSnapshot()
+    })
+  })
+
+  it('should applies specified color class when color prop is provided', () => {
+    const {getByText} = render(
+      <Typography {...mockTypographyProps} color="primary" />
+    )
+    const textContent = getByText('Test Typography')
+    expect(textContent).toHaveClass('text-primary')
+  })
+
+  it('should applies additional className when className prop is provided', () => {
+    const {getByText} = render(
+      <Typography {...mockTypographyProps} className="test-typography-class" />
+    )
+    const textContent = getByText('Test Typography')
+    expect(textContent).toHaveClass('test-typography-class')
+  })
+
+  it('should applies both color and additional className when both props are provided', () => {
+    const {getByText} = render(
+      <Typography
+        {...mockTypographyProps}
+        color="primary"
+        className="test-typography-class"
+      />
+    )
+    const textContent = getByText('Test Typography')
+    expect(textContent).toHaveClass('text-primary')
+    expect(textContent).toHaveClass('test-typography-class')
   })
 })
