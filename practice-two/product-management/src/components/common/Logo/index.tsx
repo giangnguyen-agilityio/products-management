1009d8df495eed5df9e0c2b423a52b7add6c5447
Giang Nguyen Truong <giang.nguyen@asnet.com.vn>
2023-08-07 01:20:31 +0700

    feat: implement the logo component

---
+++ practice-two/product-management/src/components/common/Logo/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Flex, Heading, Image } from '@chakra-ui/react'
+
+interface LogoProps {
+  text?: string
+  textColor?: string
+  textSize?: string
+  imageSrc?: string
+  widthSize?: string | number
+  heightSize?: string | number
+}
+
+const Logo: React.FC<LogoProps> = ({
+  text,
+  textColor = 'black',
+  textSize = 'md',
+  imageSrc,
+  widthSize = '120px',
+  heightSize = '40px',
+}) => {
+  return (
+    <Flex align="center" className="logo">
+      {/* Render the text if provided */}
+      {text && (
+        <Heading as="h1" size={textSize} color={textColor}>
+          {text}
+        </Heading>
+      )}
+      {/* Render the image if provided */}
+      {imageSrc && (
+        <Image
+          className="logo-image"
+          loading="lazy"
+          srcSet={imageSrc}
+          src={imageSrc}
+          alt="The logo website"
+          width={widthSize}
+          height={heightSize}
+        />
+      )}
+    </Flex>
+  )
+}
+
+export default Logo
