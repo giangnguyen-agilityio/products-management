e065c9fa0bce27c5427e38574fb27ba66722badc
Giang Nguyen <giang.nguyen@asnet.com.vn>
2023-08-08 17:15:20 +0700

    fix: remove the unnecessary folder

--- practice-two/product-management/src/main.tsx
+++ practice-two/product-management/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
-import './styles/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
