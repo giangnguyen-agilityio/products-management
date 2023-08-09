ed03443a2c6acd9db20c5b4da4295abfec1398d7
Giang Nguyen <giang.nguyen@asnet.com.vn>
2023-08-09 16:27:04 +0700

    feat: implement the navigation component

--- practice-two/product-management/src/main.tsx
+++ practice-two/product-management/src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
+import customThemeConfig from '@themes/custom-theme'
+import { navigationLinks } from '@constants'
+import Navigation from '@components/common/Navigation'
+import Fonts from '@themes/fonts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider></ChakraProvider>
+    <ChakraProvider theme={customThemeConfig}>
+      <Fonts />
+      <Navigation links={navigationLinks} />
+    </ChakraProvider>
   </React.StrictMode>
 )
