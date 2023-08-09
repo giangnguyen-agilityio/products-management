45ae7348fabbbee662879105cb9d4d9bba9fa9f7
Giang Nguyen <giang.nguyen@asnet.com.vn>
2023-08-09 16:26:14 +0700

    feat: implement the desktop navigation component

---
+++ practice-two/product-management/src/components/common/Navigation/DesktopNavigation/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Flex, Button } from '@chakra-ui/react'
+import { Link } from '@types'
+
+// Desktop Navigation Component
+const DesktopNavigation: React.FC<{ links: Link[] }> = ({ links }) => (
+  <Flex as="nav" className="navigation" paddingTop={1.5}>
+    <Flex as="ul" className="nav-list" gap={6}>
+      {links.map(({ label, href }) => (
+        <Flex as="li" className="nav-item" key={label}>
+          <Button
+            as="a"
+            href={href}
+            variant="ghost"
+            aria-label={label}
+            width="full"
+            fontSize="md"
+            textTransform="uppercase"
+            color="textSecondary"
+            border="1px"
+            borderColor="transparent"
+            borderRadius="full"
+            transition="all 0.4s ease-in-out"
+            _hover={{
+              bgColor: 'transparent',
+              borderColor: 'textSecondary',
+            }}
+          >
+            {label}
+          </Button>
+        </Flex>
+      ))}
+    </Flex>
+  </Flex>
+)
+
+export default DesktopNavigation
