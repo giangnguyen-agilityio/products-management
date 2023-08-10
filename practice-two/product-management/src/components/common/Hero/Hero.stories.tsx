6dd81102d10861f01b8a3972e00a0fa9d98fc0e6
Giang Nguyen <giang.nguyen@asnet.com.vn>
2023-08-10 19:49:09 +0700

    feat: implement storybook for the Hero component

---
+++ practice-two/product-management/src/components/common/Hero/Hero.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta } from '@storybook/react'
+import Hero from './index'
+
+const meta: Meta = {
+  title: 'Practice Two/Hero Component',
+  component: Hero,
+  decorators: [
+    Story => (
+      <div
+        style={{
+          backgroundColor: '#532EE7',
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+  parameters: {
+    layout: 'top',
+  },
+  tags: ['autodocs'],
+}
+
+const heroSectionContentSample = {
+  imageUrl: 'https://assets.nicepagecdn.com/d2cc3eaa/3159880/images/yttyy.jpg',
+  link: '#',
+  title: 'We help businesses and brands grow',
+  description:
+    'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident',
+}
+
+const Template = () => (
+  <Hero
+    imageUrl={heroSectionContentSample.imageUrl}
+    buttonHref={heroSectionContentSample.link}
+    title={heroSectionContentSample.title}
+    description={heroSectionContentSample.description}
+  />
+)
+
+export const Primary = Template.bind({})
+
+export default meta
