a861f3e26a621093dadbb9ff9dbd8e4b1a37e23f
Giang Nguyen <giang.nguyen@asnet.com.vn>
2023-08-10 11:33:48 +0700

    feat: implement the footer component

--- practice-two/product-management/src/main.tsx
+++ practice-two/product-management/src/main.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
+import { Box, ChakraProvider } from '@chakra-ui/react'
 import customThemeConfig from '@themes/custom-theme'
-
 import Header from '@layouts/Header'
 import Fonts from '@themes/fonts'
+import Footer from '@layouts/Footer'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={customThemeConfig}>
       <Fonts />
-      <Header />
+      <Box
+        className="container"
+        padding={{ base: '22px 10px', md: '22px 50px' }}
+      >
+        <Header />
+        <Footer />
+      </Box>
     </ChakraProvider>
   </React.StrictMode>
 )
