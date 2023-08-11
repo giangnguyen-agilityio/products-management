c3dd62232addacb5cea705a2d4a8657ed1b7f676
Giang Nguyen <giang.nguyen@asnet.com.vn>
2023-08-11 20:34:09 +0700

    fix: update the Footer component

--- practice-two/product-management/src/layouts/Footer/index.tsx
+++ practice-two/product-management/src/layouts/Footer/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Flex, Text, Link, Image, Box } from '@chakra-ui/react'
+import { Flex, Text, Link, Image } from '@chakra-ui/react'
 import Logo from '@components/common/Logo'
 import logoWebsite from '@assets/images/logo_website.webp'
 import { aboutUsLinks, footerLinks, helpLinks, socialLinks } from '@constants'
@@ -58,51 +58,59 @@ const FooterColumn: React.FC<FooterColumnProps> = ({ title, links }) => (
 
 const Footer: React.FC = () => {
   return (
-    <Box as="footer" className="footer" width="full">
-      <Flex
-        flexDirection={{ base: 'column-reverse', lg: 'row' }}
-        justifyContent="space-between"
-        alignItems="flex-start"
-      >
-        {/* Footer left */}
-        <Flex className="footer-left" flexDirection="column">
-          <Logo imageSrc={logoWebsite} />
-          <Text
-            as="span"
-            pt={2}
-            pl={4}
-            color="textSecondary"
-            fontSize={{ base: 'sm', md: 'md' }}
-          >
-            Copyright &copy; 2023, Giang Nguyen
-            <br />
-            All rights reserved.
-          </Text>
-          {/* Social media links */}
-          <Flex className="link-social" pt={1} pl={2.5}>
-            {socialLinks.map((link, index) => (
-              <FooterButton key={index} icon={link.icon} href={link.href} />
-            ))}
-          </Flex>
-        </Flex>
-
-        {/* Footer right */}
-        <Flex
-          className="footer-right"
-          marginLeft={{ base: 4, xl: 0 }}
-          gap={{ base: 8, xl: 16 }}
+    <Flex
+      as="footer"
+      className="footer"
+      maxWidth={{
+        sm: '340px',
+        md: '720px',
+        lg: '940px',
+        xl: '1140px',
+      }}
+      margin="0 auto"
+      flexDirection={{ base: 'column-reverse', lg: 'row' }}
+      justifyContent="space-between"
+      alignItems="flex-start"
+      gap={{ base: 2, sm: 8 }}
+    >
+      {/* Footer left */}
+      <Flex className="footer-left" flexDirection="column">
+        <Logo imageSrc={logoWebsite} />
+        <Text
+          as="span"
+          pt={2}
+          pl={4}
+          color="textSecondary"
+          fontSize={{ base: 'sm', md: 'md' }}
         >
-          {/* Column 1 */}
-          <FooterColumn title="Discover" links={footerLinks} />
-
-          {/* Column 2 */}
-          <FooterColumn title="About Us" links={aboutUsLinks} />
-
-          {/* Column 3 */}
-          <FooterColumn title="Help" links={helpLinks} />
+          Copyright &copy; 2023, Giang Nguyen
+          <br />
+          All rights reserved.
+        </Text>
+        {/* Social media links */}
+        <Flex className="link-social" pt={1} pl={2.5}>
+          {socialLinks.map((link, index) => (
+            <FooterButton key={index} icon={link.icon} href={link.href} />
+          ))}
         </Flex>
       </Flex>
-    </Box>
+
+      {/* Footer right */}
+      <Flex
+        className="footer-right"
+        marginLeft={{ base: 4, xl: 0 }}
+        gap={{ base: 8, xl: 16 }}
+      >
+        {/* Column 1 */}
+        <FooterColumn title="Discover" links={footerLinks} />
+
+        {/* Column 2 */}
+        <FooterColumn title="About Us" links={aboutUsLinks} />
+
+        {/* Column 3 */}
+        <FooterColumn title="Help" links={helpLinks} />
+      </Flex>
+    </Flex>
   )
 }
 
