51ca137c7167050d0cf273eef24bbcd2f658337f
Giang Nguyen Truong <giang.nguyen@asnet.com.vn>
2023-08-15 01:22:40 +0700

    feat: implement the product detail page

---
+++ practice-two/product-management/src/pages/ProductDetailPage/index.tsx
@@ -0,0 +1,8 @@
+import { memo } from 'react'
+import ProductDetail from '@components/ProductDetail'
+
+const ProductDetailPage = () => {
+  return <ProductDetail />
+}
+
+export default memo(ProductDetailPage)
