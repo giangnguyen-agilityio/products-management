4908657145e28341f8555706a9f690e05bf127d5
Giang Nguyen <giang.nguyen@asnet.com.vn>
2023-08-15 19:28:52 +0700

    feat: implement the ProductDetai page

--- practice-two/product-management/src/main.tsx
+++ practice-two/product-management/src/main.tsx
@@ -1,35 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Box, ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import customThemeConfig from '@themes/custom-theme'
-import Header from '@layouts/Header'
 import Fonts from '@themes/fonts'
-import Footer from '@layouts/Footer'
-import Hero from '@components/common/Hero'
-import { heroSectionContent } from '@constants'
-import ProductList from '@components/ProductList'
-import Contact from '@components/Contact'
+import { RouterProvider } from 'react-router-dom'
+import { routes } from './routes/index'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={customThemeConfig}>
       <Fonts />
-      <Box
-        className="container"
-        padding={{ base: '22px 10px', md: '22px 50px' }}
-        overflowY={{ base: 'hidden', xs: 'scroll' }}
-      >
-        <Header />
-        <Hero
-          imageUrl={heroSectionContent.imageUrl}
-          buttonHref={heroSectionContent.link}
-          title={heroSectionContent.title}
-          description={heroSectionContent.description}
-        />
-        <ProductList />
-        <Contact />
-        <Footer />
-      </Box>
+      <RouterProvider router={routes} />
     </ChakraProvider>
   </React.StrictMode>
 )
