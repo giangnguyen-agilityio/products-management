70c202f78e0480dcb1bb972464c53a3a65f29687
Giang Nguyen <giang.nguyen@asnet.com.vn>
2023-08-15 19:24:56 +0700

    feat: implement ProductDesc component

--- practice-two/product-management/src/components/ProductDetail/ProductDesc/index.tsx
+++ practice-two/product-management/src/components/ProductDetail/ProductDesc/index.tsx
@@ -1,20 +1,23 @@
 import React, { memo } from 'react'
 import { Box, Text } from '@chakra-ui/react'
 
-const ProductDesc: React.FC = () => (
-  <Box marginTop={4}>
-    <Text fontSize="lg" fontWeight="bold" color="secondary">
-      Overview
-    </Text>
-    <Text marginTop={2} color="textTertiary" fontFamily="OpenSans-Regular">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dignissim
-      nisl eu nunc interdum, eget luctus ligula malesuada. Lorem ipsum dolor sit
-      amet, consectetur adipiscing elit. Nullam dignissim nisl eu nunc interdum,
-      eget luctus ligula malesuada. Lorem ipsum dolor sit amet, consectetur
-      adipiscing elit. Nullam dignissim nisl eu nunc interdum, eget luctus
-      ligula malesuada.
-    </Text>
-  </Box>
-)
+interface ProductDescProp {
+  productDesc: string
+}
+
+const ProductDesc: React.FC<ProductDescProp> = props => {
+  const { productDesc } = props
+
+  return (
+    <Box marginTop={4}>
+      <Text fontSize="lg" fontWeight="bold" color="secondary">
+        Overview
+      </Text>
+      <Text marginTop={2} color="textTertiary" fontFamily="OpenSans-Regular">
+        {productDesc}
+      </Text>
+    </Box>
+  )
+}
 
 export default memo(ProductDesc)
