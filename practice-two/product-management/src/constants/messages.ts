741b57ab01b57d023ce0e9522894b13742acee6f
Giang Nguyen <giang.nguyen@asnet.com.vn>
2023-08-16 19:45:50 +0700

    feat: implement the Form component

---
+++ practice-two/product-management/src/constants/messages.ts
@@ -0,0 +1,7 @@
+export const ERROR_MESSAGES = {
+  NAME_IS_MISSING: 'Please enter the name of product',
+  VALUE_IS_NOT_VALID: 'Please enter a value greater than 0',
+  RATE_NUMBER_IS_NOT_VALID: 'Please enter a value between 0 and 5',
+  DESCRIPTION_IS_MISSING: 'Please enter the description of product',
+  PRODUCT_IMAGE_IS_MISSING: 'Please select an image',
+}
