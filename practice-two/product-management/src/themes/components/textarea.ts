05a4ca4277016e34fbd82bc9d4c935098ef55246
Giang Nguyen <giang.nguyen@asnet.com.vn>
2023-08-18 18:08:31 +0700

    feat: custom the Textarea component

---
+++ practice-two/product-management/src/themes/components/textarea.ts
@@ -0,0 +1,20 @@
+import { ComponentStyleConfig } from '@chakra-ui/react'
+
+export const Textarea: ComponentStyleConfig = {
+  variants: {
+    primary: {
+      border: 'none',
+      background: 'background',
+      borderBottom: '2px solid',
+      borderColor: 'textPrimary',
+      borderRadius: 'none',
+      outline: 'none',
+      _hover: {
+        borderBottomColor: 'textPrimary',
+      },
+      _placeholder: {
+        color: 'gray.500',
+      },
+    },
+  },
+}
