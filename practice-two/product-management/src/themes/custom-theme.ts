d452132b19776d03e912b5af7a1a90316051e86b
Giang Nguyen Truong <giang.nguyen@asnet.com.vn>
2023-08-20 02:50:09 +0700

    feat: custom the box shadows

--- practice-two/product-management/src/themes/custom-theme.ts
+++ practice-two/product-management/src/themes/custom-theme.ts
@@ -2,12 +2,14 @@ import { extendTheme } from '@chakra-ui/react'
 import { breakpoints } from './breakpoint'
 import { colors } from './colors'
 import { fonts } from './fonts'
+import { shadows } from './shadows'
 import { Badge } from './components/badge'
 import { Text } from './components/text'
 import { Textarea } from './components/textarea'
 import { Input } from './components/input'
 import { Button } from './components/button'
 import { Container } from './components/container'
+import { Spinner } from './components/spinner'
 
 const customThemeConfig = extendTheme({
   styles: {
@@ -23,13 +25,16 @@ const customThemeConfig = extendTheme({
   breakpoints,
   colors,
   fonts,
+  shadows,
   components: {
     Badge,
     Text,
     Input,
     Textarea,
     Button,
+
     Container,
+    Spinner,
   },
 })
 
---
+++ practice-two/product-management/src/themes/shadows.ts
@@ -0,0 +1,6 @@
+export const shadows = {
+  default: `5px -6px 20px 0 rgba(0,0,0,0.25)`,
+  primary: `-2px -2px 5px -4px`,
+  secondary: `rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 20px 20px 12px`,
+  tertiary: `5px 5px 29px 0 rgba(0,0,0,0.15)`,
+}
