61ef216795761acb43d99618f9872110f72dce5f
Giang Nguyen <giang.nguyen@asnet.com.vn>
2023-08-21 14:21:13 +0700

    fix: update the main layout

--- practice-two/product-management/src/layouts/MainLayout/index.tsx
+++ practice-two/product-management/src/layouts/MainLayout/index.tsx
@@ -1,18 +1,32 @@
+import { Suspense } from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '@layouts/Header'
 import Footer from '@layouts/Footer'
 import { Box } from '@chakra-ui/react'
 import ProductProvider from '@stores/products/ProductProvider'
+import Loading from '@components/common/Loading'
 import { ErrorBoundary } from '@components/ErrorBoundary'
+import Hero from '@components/common/Hero'
+import { heroSectionContent } from '@constants'
+import Contact from '@components/Contact'
 
 const MainLayout = (): JSX.Element => {
   return (
     <ErrorBoundary>
       <Box padding={{ base: '22px 10px', md: '22px 50px' }} overflow="hidden">
         <Header />
-        <ProductProvider>
-          <Outlet />
-        </ProductProvider>
+        <Hero
+          imageUrl={heroSectionContent.imageUrl}
+          buttonHref={heroSectionContent.link}
+          title={heroSectionContent.title}
+          description={heroSectionContent.description}
+        />
+        <Suspense fallback={<Loading />}>
+          <ProductProvider>
+            <Outlet />
+          </ProductProvider>
+        </Suspense>
+        <Contact />
         <Footer />
       </Box>
     </ErrorBoundary>
