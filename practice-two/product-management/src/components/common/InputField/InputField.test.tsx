80718b1f03541efd6bb5586a59b03c5eaf08d466
Giang Nguyen <giang.nguyen@asnet.com.vn>
2023-08-22 14:20:49 +0700

    fix: update the deep compare for the InputField component

--- practice-two/product-management/src/components/common/InputField/InputField.test.tsx
+++ practice-two/product-management/src/components/common/InputField/InputField.test.tsx
@@ -1,13 +1,16 @@
-import { render, fireEvent } from '@testing-library/react'
-import InputField from './'
+import { render, screen, fireEvent } from '@testing-library/react'
+import InputField, { arePropsEqual } from './'
 
-describe('InputField components', () => {
-  const onChangeMock = jest.fn()
-
+describe('InputField Component', () => {
   const defaultProps = {
-    name: 'testField',
-    label: 'Test Field',
-    onChange: onChangeMock,
+    name: 'test',
+    label: 'Test Label',
+    value: 'initial value',
+    onChange: jest.fn(),
+    errorMessage: 'Error message',
+    isReadOnly: false,
+    min: 0,
+    max: 10,
   }
 
   it('renders the component with default props', () => {
@@ -32,7 +35,7 @@ describe('InputField components', () => {
     const input = getByLabelText('Input Field')
 
     fireEvent.change(input, { target: { value: 'New Value' } })
-    expect(onChangeMock).toHaveBeenCalledTimes(1)
+    expect(defaultProps.onChange).toHaveBeenCalledTimes(1)
   })
 
   it('renders a textarea when variant is set to "textarea"', () => {
@@ -41,5 +44,75 @@ describe('InputField components', () => {
     )
     const textarea = getByLabelText('Input Field')
     expect(textarea.tagName.toLowerCase()).toBe('textarea')
+    expect(textarea).toBeInTheDocument()
+    expect(textarea).toHaveStyle('height: 100px')
+  })
+
+  it('renders error message when errorMessage prop is provided', () => {
+    render(<InputField {...defaultProps} />)
+    const errorMessage = screen.getByText(defaultProps.errorMessage)
+    expect(errorMessage).toBeInTheDocument()
+  })
+
+  it('renders with correct input type', () => {
+    render(<InputField {...defaultProps} type="number" />)
+    const input = screen.getByLabelText('Input Field')
+    expect(input).toHaveAttribute('type', 'number')
+  })
+
+  it('displays readOnly input when isReadOnly prop is true', () => {
+    render(<InputField {...defaultProps} isReadOnly />)
+    const input = screen.getByLabelText('Input Field')
+    expect(input).toHaveAttribute('readonly')
+  })
+
+  it('renders input with min and max attributes when provided', () => {
+    render(<InputField {...defaultProps} />)
+    const input = screen.getByLabelText('Input Field')
+    expect(input).toHaveAttribute('min', defaultProps.min.toString())
+    expect(input).toHaveAttribute('max', defaultProps.max.toString())
+  })
+
+  it('does not render error message when errorMessage prop is not provided', () => {
+    render(<InputField {...defaultProps} errorMessage="" />)
+    const errorMessage = screen.queryByText(defaultProps.errorMessage)
+    expect(errorMessage).toBeNull()
+  })
+
+  it('should return true for equal props', () => {
+    const prevProps = {
+      name: 'username',
+      label: 'Username',
+      value: 'testuser',
+      errorMessage: 'Invalid input',
+      isReadOnly: false,
+      min: 0,
+      max: 10,
+      onChange: () => {},
+    }
+
+    const nextProps = { ...prevProps }
+
+    expect(arePropsEqual(prevProps, nextProps)).toBe(true)
+  })
+
+  it('should return false for different props', () => {
+    const prevProps = {
+      name: 'username',
+      label: 'Username',
+      value: 'testuser',
+      errorMessage: 'Invalid input',
+      isReadOnly: false,
+      min: 0,
+      max: 10,
+      onChange: () => {},
+    }
+
+    const nextProps = {
+      ...prevProps,
+      value: 'updateduser',
+    }
+
+    expect(arePropsEqual(prevProps, nextProps)).toBe(false)
   })
 })
--- practice-two/product-management/src/components/common/InputField/index.tsx
+++ practice-two/product-management/src/components/common/InputField/index.tsx
@@ -1,5 +1,10 @@
 import React, { InputHTMLAttributes, memo } from 'react'
-import { FormControl, FormLabel, Input, Text } from '@chakra-ui/react'
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+} from '@chakra-ui/react'
 
 // Define the props interface for the InputField component
 interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
@@ -15,6 +20,23 @@ interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
   max?: number
 }
 
+// Custom deep comparison function for props
+export function arePropsEqual(
+  prevProps: InputFieldProps,
+  nextProps: InputFieldProps
+) {
+  return (
+    prevProps.variant === nextProps.variant &&
+    prevProps.name === nextProps.name &&
+    prevProps.label === nextProps.label &&
+    prevProps.value === nextProps.value &&
+    prevProps.errorMessage === nextProps.errorMessage &&
+    prevProps.isReadOnly === nextProps.isReadOnly &&
+    prevProps.min === nextProps.min &&
+    prevProps.max === nextProps.max
+  )
+}
+
 const InputField: React.FC<InputFieldProps> = ({
   variant = 'input',
   name,
@@ -27,7 +49,7 @@ const InputField: React.FC<InputFieldProps> = ({
   min,
   max,
 }) => (
-  <FormControl className="input-field" isRequired>
+  <FormControl className="input-field" isRequired isInvalid={!!errorMessage}>
     {/* Form Label */}
     <FormLabel
       className="form-label"
@@ -40,9 +62,9 @@ const InputField: React.FC<InputFieldProps> = ({
 
     {/* Input field */}
     <Input
+      as={variant}
       readOnly={isReadOnly}
       variant="primary"
-      as={variant}
       padding={2}
       height={variant === 'textarea' ? '100px' : '45px'}
       type={type}
@@ -55,8 +77,10 @@ const InputField: React.FC<InputFieldProps> = ({
     />
 
     {/* Error message */}
-    {errorMessage != null && <Text variant="danger">{errorMessage}</Text>}
+    {errorMessage && (
+      <FormErrorMessage color="error">{errorMessage}</FormErrorMessage>
+    )}
   </FormControl>
 )
 
-export default memo(InputField)
+export default memo(InputField, arePropsEqual)
