270d6c7d16520aa2e4ba6f251e8a8d13c9b3000d
Giang Nguyen <giang.nguyen@asnet.com.vn>
2023-08-25 19:02:08 +0700

    fix: update the ProductList component

--- practice-two/product-management/src/components/ProductList/index.tsx
+++ practice-two/product-management/src/components/ProductList/index.tsx
@@ -1,50 +1,20 @@
-import React, { useContext } from 'react'
-import {
-  Box,
-  Text,
-  Flex,
-  Button,
-  Grid,
-  Image,
-  Container,
-} from '@chakra-ui/react'
-
-import arrowDownIcon from '@assets/icons/Arrow_down.svg'
-import ProductListHeader from './ProductListHeader'
+import React from 'react'
+import { Grid, Container } from '@chakra-ui/react'
 import ProductItem from '@components/ProductItem'
-import ProductContext from '@stores/products/ProductContext'
-import { IProduct } from '@types'
 import { NOTIFICATIONS } from '@constants'
 import EmptyProduct from '@components/common/EmptyProduct'
 import { memo } from 'react'
-import ProductListControl from './ProductListControl'
+import { IProduct } from '@types'
 
 interface ProductListProps {
-  openModal: () => void
-  onClickShowMore: () => void
-  isVisible: boolean
+  listProduct: IProduct[]
 }
 
-const ProductList: React.FC<ProductListProps> = ({
-  openModal,
-  onClickShowMore,
-  isVisible,
-}) => {
-  const { productState } = useContext(ProductContext)
-  const productList: IProduct[] = productState.products
-  const isProductListNotEmpty = productList.length > 0
+const ProductList: React.FC<ProductListProps> = ({ listProduct }) => {
+  const isProductListNotEmpty = listProduct.length > 0
 
   return (
-    <Box
-      as="section"
-      className="product-list-section"
-      width="full"
-      margin="0 auto 150px"
-      padding={{ base: '0 50px', md: '0' }}
-    >
-      <ProductListHeader />
-      <ProductListControl onOpenModal={openModal} />
-
+    <>
       {isProductListNotEmpty ? (
         <Container padding={0}>
           <Grid
@@ -59,50 +29,15 @@ const ProductList: React.FC<ProductListProps> = ({
             margin={{ base: '0 -50px', sm: '0 auto' }}
             rowGap={{ md: '0' }}
           >
-            {productList.map(product => (
+            {listProduct.map(product => (
               <ProductItem key={product.id} {...{ product }} />
             ))}
           </Grid>
-
-          <Flex justifyContent="center" margin="50px auto">
-            {isVisible && (
-              <Button
-                onClick={onClickShowMore}
-                className="load-more-btn"
-                name="button"
-                aria-label="Button for loading more products"
-                fontFamily="Oswald-Regular"
-                padding="12px 40px"
-                variant="tertiary"
-                height="fit-content"
-              >
-                <Image
-                  src={arrowDownIcon}
-                  width="20px"
-                  height="20px"
-                  alt="Arrow down icon"
-                  margin="0 auto"
-                  loading="eager"
-                />
-                <Text
-                  as="span"
-                  display="flex"
-                  color="textSecondary"
-                  justifyContent="center"
-                  fontSize="18px"
-                  textTransform="uppercase"
-                  marginLeft={2}
-                >
-                  load more
-                </Text>
-              </Button>
-            )}
-          </Flex>
         </Container>
       ) : (
         <EmptyProduct errorMessage={NOTIFICATIONS.NOT_FOUND_PRODUCT} />
       )}
-    </Box>
+    </>
   )
 }
 
