c19d9cff2c6606f299ea46aa320842561a5ea5f4
Giang Nguyen <giang.nguyen@asnet.com.vn>
2023-08-29 17:57:23 +0700

    fix: update unit test for the Error Boundary

--- practice-two/product-management/src/components/ErrorBoundary/ErrorBoundary.test.tsx
+++ practice-two/product-management/src/components/ErrorBoundary/ErrorBoundary.test.tsx
@@ -1,17 +1,56 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { ErrorBoundary } from './'
+import { NOTIFICATIONS } from '@constants'
 
-describe('ErrorBoundary Component', () => {
-  // Mock children components
-  const MockChild = () => <div>Mock Child Component</div>
+describe('ErrorBoundary', () => {
+  it('should render children when there is no error', () => {
+    const ChildComponent = () => <div>Child Component</div>
 
-  it('renders children when no error is caught', () => {
-    const { getByText } = render(
+    render(
       <ErrorBoundary>
-        <MockChild />
+        <ChildComponent />
       </ErrorBoundary>
     )
 
-    expect(getByText('Mock Child Component')).toBeInTheDocument()
+    const childElement = screen.getByText('Child Component')
+    expect(childElement).toBeInTheDocument()
+  })
+
+  it('should render error message and image when an error is caught', () => {
+    const ThrowErrorComponent = () => {
+      throw new Error('Test error')
+    }
+
+    render(
+      <ErrorBoundary>
+        <ThrowErrorComponent />
+      </ErrorBoundary>
+    )
+
+    const errorMessage = screen.getByText(NOTIFICATIONS.API_ERROR)
+    expect(errorMessage).toBeInTheDocument()
+
+    const errorImage = screen.getByAltText('This is the error image')
+    expect(errorImage).toBeInTheDocument()
+  })
+
+  it('should render a link to the homepage when there is an error', () => {
+    const ThrowErrorComponent = () => {
+      throw new Error('Test error')
+    }
+
+    render(
+      <ErrorBoundary>
+        <ThrowErrorComponent />
+      </ErrorBoundary>
+    )
+
+    const link = screen.getByRole('link', {
+      name: NOTIFICATIONS.BACK_TO_HOMEPAGE,
+    })
+    expect(link).toBeInTheDocument()
+
+    userEvent.click(link)
   })
 })
