4c515a3f6f6238c8593073287dc22f045b0693b7
Giang Nguyen Truong <giang.nguyen@asnet.com.vn>
2023-09-11 02:05:23 +0700

    fix: update the performance score

--- practice-two/product-management/vite.config.ts
+++ practice-two/product-management/vite.config.ts
@@ -1,11 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'node_modules'),
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   build: {
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+        manualChunks: {
+          'react-dom': ['react-dom'],
+          'react-router-dom': ['react-router-dom'],
+          '@chakra-ui/icons': ['@chakra-ui/icons'],
+          '@chakra-ui/react': ['@chakra-ui/react'],
+          'framer-motion': ['framer-motion'],
+        },
+      },
+    },
   },
 })
